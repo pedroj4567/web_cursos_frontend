bac2135b178bb5054b91b5c6b7da119b0b4a1a29
pedroj4567 <pedroj4567@gmail.com>
2025-04-14 13:44:37 -0500

    comment pager

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,5 @@ import LandingPage from "./LandingPage";
 import LoginPage from "./auth/LoginPage";
 import SignupPage from "./auth/SignupPage";
 import RecoverPassword from "./auth/RecoverPassword";
-import ChangePassword from "./auth/ChangePassword";
-export { LandingPage, LoginPage, SignupPage, RecoverPassword, ChangePassword };
+// import ChangePassword from "./auth/ChangePassword";
+export { LandingPage, LoginPage, SignupPage, RecoverPassword };
